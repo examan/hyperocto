eba79d54e1622f24cca1f5936981e73e670bbc91
Megaman <megaman@megaman.twgogo.org>
2016-11-12 13:12:16 +0800

    as same as previous commit

--- src/background-script.js
+++ src/background-script.js
@@ -1,62 +1,62 @@
-"use strict";
+/* global MESSAGETYPE, OPENMODELIST, OPENMODE, enumerationBuilder */
 
-const CONTEXTMENUIDLIST = [];
-for(let openmode of OPENMODELIST) {
-	CONTEXTMENUIDLIST.push(`OPEN_${openmode}`);
+'use strict'
+
+const CONTEXTMENUIDLIST = []
+for (let openmode of OPENMODELIST) {
+  CONTEXTMENUIDLIST.push(`OPEN_${openmode}`)
 }
-const CONTEXTMENU = CONTEXTMENUIDLIST.enumerationBuilder();
+const CONTEXTMENU = enumerationBuilder(CONTEXTMENUIDLIST)
 
-for(let menuItemId of CONTEXTMENUIDLIST) {
-	chrome.contextMenus.create({
-		id: menuItemId,
-		title: chrome.i18n.getMessage(`CONTEXTMENU_${menuItemId}`),
-		contexts: ["link"]
-	});
+for (let menuItemId of CONTEXTMENUIDLIST) {
+  chrome.contextMenus.create({
+    id: menuItemId,
+    title: chrome.i18n.getMessage(`CONTEXTMENU_${menuItemId}`),
+    contexts: ['link']
+  })
 }
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-	switch (info.menuItemId) {
-	case CONTEXTMENU.OPEN_FOLLOWING:
-	case CONTEXTMENU.OPEN_ALL:
-		let mode = ((menuItemId) => {
-			switch (menuItemId) {
-				case CONTEXTMENU.OPEN_FOLLOWING:
-					return OPENMODE.FOLLOWING;
-				case CONTEXTMENU.OPEN_ALL:
-					return OPENMODE.ALL;
-			}
-		})(info.menuItemId);
+  switch (info.menuItemId) {
+    case CONTEXTMENU.OPEN_FOLLOWING:
+    case CONTEXTMENU.OPEN_ALL:
+      let mode = (menuItemId => {
+        switch (menuItemId) {
+          case CONTEXTMENU.OPEN_FOLLOWING:
+            return OPENMODE.FOLLOWING
+          case CONTEXTMENU.OPEN_ALL:
+            return OPENMODE.ALL
+        }
+      })(info.menuItemId)
 
-		chrome.tabs.sendMessage(
-			tab.id,
-			{
-				"type": MESSAGETYPE.OPEN,
-				"mode": mode,
-				"fromTabIndex": tab.index
-			},
-			{
-				frameId: info.frameId
-			},
-			(message) => {
-				if(!message) {
-					return;
-				}
+      chrome.tabs.sendMessage(
+        tab.id,
+        {
+          'type': MESSAGETYPE.OPEN,
+          'mode': mode,
+          'fromTabIndex': tab.index
+        },
+        {
+          frameId: info.frameId
+        },
+        message => {
+          if (!message) {
+            return
+          }
 
-				message.urls.forEach((url, index) => {
-					setTimeout(
-						(param) => {
-							chrome.tabs.create(param);
-						},
-						0,
-						{
-							"url": url,
-							"index": message.fromTabIndex + index + 1,
-							"active": false
-						}
-					);
-				});
-			}
-		);
-		break;
-	}
-});
+          message.urls.forEach((url, index) => {
+            setTimeout(
+              param => chrome.tabs.create(param),
+              0,
+              {
+                'url': url,
+                'index': message.fromTabIndex + index + 1,
+                'active': false
+              }
+            )
+          })
+        }
+      )
+      break
+  }
+})
--- src/content-script.js
+++ src/content-script.js
@@ -1,143 +1,105 @@
-"use strict";
+/* global MESSAGETYPE, OPENMODE */
 
-function getPathElements(targetElement) {
-	let element = targetElement;
-	let elements = [targetElement];
-	while (element = element.parentElement) {
-		elements.unshift(element);
-	}
-	return elements;
+'use strict'
+
+function getPathElements (targetElement) {
+  let element = targetElement
+  let elements = [targetElement]
+  while ((element = element.parentElement)) {
+    elements.unshift(element)
+  }
+  return elements
 }
 
-function getSimilarPathElements(targetElement) {
-	const uriSchemeFilterSelector = "about chrome opera javascript".split(" ").map((uriScheme) => {
-		return `:not([href^="${uriScheme}:"])`;
-	}).join('');
+function getSimilarPathElements (targetElement) {
+  const uriSchemeFilterSelector = 'about chrome opera javascript'
+    .split(' ')
+    .map((uriScheme) => `:not([href^='${uriScheme}:'])`)
+    .join('')
 
-	let elements = getPathElements(targetElement);
-	let pathSelector = elements.map((element) => {
-		return element.tagName;
-	}).join(" > ");
-	let elementSelector = `${pathSelector}${uriSchemeFilterSelector}`;
-	let elementList = document.querySelectorAll(elementSelector);
+  let elements = getPathElements(targetElement)
+  let pathSelector = elements
+    .map(element => element.tagName)
+    .join(' > ')
+  let elementSelector = `${pathSelector}${uriSchemeFilterSelector}`
+  let elementList = document.querySelectorAll(elementSelector)
 
-	/*
-	let tempClass = `__hyperocto__${Date.now()}`;
-
-	elements.forEach((element) => {
-		element.classList.add(tempClass);
-	});
-
-	let elementList = null;
-	elements.slice().reverse().every((element) => {
-		let pathSelector = elements.map((element) => {
-			if(element.classList.contains(tempClass)) {
-				return `${element.tagName}.${tempClass}`;
-			} else {
-				return element.tagName;
-			}
-		}).join(" > ");
-		let elementSelector = `${pathSelector}${uriSchemeFilterSelector}`;
-		let elementList = document.querySelectorAll(elementSelector);
-
-		if(1 < elementList.length) {
-			//break;
-			return false;
-		}
-
-		element.classList.remove(tempClass);
-		//continue;
-		return true;
-	});
-
-	elements.forEach((element) => {
-		element.classList.remove(tempClass);
-	});
-	*/
-
-	return Array.prototype.slice.call(elementList);
+  return Array.prototype.slice.call(elementList)
 }
 
-function getSimilarLinks(targetLink, mode) {
-	const similarStyleNames = "display font lineHeight position verticalAlign".split(" ");
+function getSimilarLinks (targetLink, mode) {
+  const similarStyleNames = 'display font lineHeight position verticalAlign'.split(' ')
 
-	let links = getSimilarPathElements(targetLink);
+  let links = getSimilarPathElements(targetLink)
 
-	if(mode === OPENMODE.FOLLOWING) {
-		links.splice(0, links.indexOf(targetLink));
-	}
+  if (mode === OPENMODE.FOLLOWING) {
+    links.splice(0, links.indexOf(targetLink))
+  }
 
-	let targetLinkStyle = getComputedStyle(targetLink);
+  let targetLinkStyle = window.getComputedStyle(targetLink)
 
-	// filter similar style element
-	links = links.filter((link) => {
-		let linkStyle = getComputedStyle(link);
+  // filter similar style element
+  links = links.filter(link => {
+    let linkStyle = window.getComputedStyle(link)
 
-		return similarStyleNames.every((styleName) => {
-			return linkStyle[styleName] === targetLinkStyle[styleName];
-		});
-	});
+    return similarStyleNames.every(styleName => linkStyle[styleName] === targetLinkStyle[styleName])
+  })
 
-	// filter zero-dimensioned element
-	links = links.filter((link) => {
-		let element = link;
+  // filter zero-dimensioned element
+  links = links.filter(link => {
+    let element = link
 
-		while(element) {
-			if (element.offsetWidth !== 0 && element.offsetHeight !== 0 && element.getClientRects().length !== 0) {
-				return true;
-			}
+    while (element) {
+      if (element.offsetWidth !== 0 && element.offsetHeight !== 0 && element.getClientRects().length !== 0) {
+        return true
+      }
 
-			if(element.firstChild) {
-				element = element.firstChild;
-			} else if (element === link) {
-				// no child
-				return false;
-			} else if (element.nextSibling) {
-				element = element.nextSibling;
-			} else if(element.parentNode != link) {
-				element = element.parentNode;
-			} else {
-				// no dimensioned child
-				return false;
-			}
-		}
+      if (element.firstChild) {
+        element = element.firstChild
+      } else if (element === link) {
+        // no child
+        return false
+      } else if (element.nextSibling) {
+        element = element.nextSibling
+      } else if (element.parentNode !== link) {
+        element = element.parentNode
+      } else {
+        // no dimensioned child
+        return false
+      }
+    }
 
-		// edge case ?
-		return false;
-	});
+    // edge case ?
+    return false
+  })
 
-	return links;
+  return links
 }
 
-function contextMenuOpenMessageListener(message, sender, sendResponse) {
-	const THRESHOLD_TAB_CREATE_CONFIRM = 16;
+function contextMenuOpenMessageListener (message, sender, sendResponse) {
+  const THRESHOLD_TAB_CREATE_CONFIRM = 16
 
-	let targetLink = document.activeElement;
-	let links = getSimilarLinks(targetLink, message.mode);
+  let targetLink = document.activeElement
+  let links = getSimilarLinks(targetLink, message.mode)
 
-	let filteredLink = links;
+  // Confirm dialog is not avaiable in background script in Firefox.
+  let linkCount = links.length
+  if (THRESHOLD_TAB_CREATE_CONFIRM <= linkCount && !window.confirm(chrome.i18n.getMessage('CONFIRM_OPEN', [linkCount]))) {
+    return
+  }
 
-	// Confirm dialog is not avaiable in background script in Firefox.
-	let linkCount = links.length;
-	if (THRESHOLD_TAB_CREATE_CONFIRM <= linkCount && !confirm(chrome.i18n.getMessage("CONFIRM_OPEN", [linkCount]))) {
-		return;
-	}
-
-	let urls = links.map((link) => {
-		return link.href;
-	});
-	sendResponse({
-		"fromTabIndex": message.fromTabIndex,
-		"urls": urls
-	});
+  let urls = links.map(link => link.href)
+  sendResponse({
+    'fromTabIndex': message.fromTabIndex,
+    'urls': urls
+  })
 }
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-	switch(message.type) {
-	case MESSAGETYPE.OPEN:
-		return contextMenuOpenMessageListener.call(this, message, sender, sendResponse);
-		break;
-	default:
-		return;
-	}
-});
+  switch (message.type) {
+    case MESSAGETYPE.OPEN:
+      return contextMenuOpenMessageListener.call(this, message, sender, sendResponse)
+    default:
+      return
+  }
+})
