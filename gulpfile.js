a3d9414ceea94a9380f835ae7b96f8b5c0a7014c
Megaman <megaman@megaman.twgogo.org>
2016-11-24 01:05:19 +0800

    Add babili

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 let gulp = require('gulp')
 var merge = require('merge-stream')
 let eslint = require('gulp-eslint')
+let babel = require("gulp-babel");
 let zip = require('gulp-zip');
 
 gulp.task('default', function () {
@@ -13,7 +14,11 @@ gulp.task('default', function () {
     gulp.src(['src/**/*.js', '!src/lib/browser-polyfill.js'])
       .pipe(eslint())
       .pipe(eslint.formatEach())
-      .pipe(eslint.failAfterError()),
+      .pipe(eslint.failAfterError())
+      .pipe(babel({
+        'comments': false,
+        'presets': ['babili']
+      })),
 
     gulp.src(['src/**/*', '!src/**/*.js'])
 
