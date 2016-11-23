8d275cd44d3337d0f45b8aa6a94aec39998816fe
Megaman <megaman@megaman.twgogo.org>
2016-11-24 01:36:14 +0800

    Fix gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,13 @@
 let gulp = require('gulp')
 var merge = require('merge-stream')
 let eslint = require('gulp-eslint')
-let babel = require("gulp-babel");
-let zip = require('gulp-zip');
+let babel = require('gulp-babel')
+let zip = require('gulp-zip')
 
 gulp.task('default', function () {
-  let pkg = require('./package.json')
-
   return merge(
 
-    gulp.src(['src/**/*.js', '!src/lib/browser-polyfill.js'])
+    gulp.src('src/js/*.js', {base: 'src/'})
       .pipe(eslint())
       .pipe(eslint.formatEach())
       .pipe(eslint.failAfterError())
@@ -20,7 +18,7 @@ gulp.task('default', function () {
         'presets': ['babili']
       })),
 
-    gulp.src(['src/**/*', '!src/**/*.js', 'src/lib/browser-polyfill.js'])
+    gulp.src(['src/**/*', '!src/js/*.js'], {base: 'src/'})
 
   ).pipe(zip('hyperocto.zip'))
   .pipe(gulp.dest('dest'))
